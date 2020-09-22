0363b5642a83ae288716b542b21890e1133fa07b
mumusia <109430387+mumusia@users.noreply.github.com>
2020-09-22 15:34:15 +0000

    Update online.md

--- app/src/androidTest/java/com/example/flipcoin/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/flipcoin/ExampleInstrumentedTest.kt
@@ -3,3 +3,6 @@ package com.example.flipcoin
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
