86194d265fe431868fd5f5d8cb0d72a1ededc19a
mumusia <109430387+mumusia@users.noreply.github.com>
2020-09-25 10:04:59 +0000

    standardize

--- app/src/androidTest/java/com/example/flipcoin/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/flipcoin/ExampleInstrumentedTest.kt
@@ -12,3 +12,9 @@ import org.junit.Assert.*
 /**
  * Instrumented test, which will execute on an Android device.
  *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
