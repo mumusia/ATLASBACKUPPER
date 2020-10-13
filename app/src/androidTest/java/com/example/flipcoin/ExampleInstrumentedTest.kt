6a7a7868f8bf7aeb83fbd57bb29d947dc3c967ba
mumusia <109430387+mumusia@users.noreply.github.com>
2020-10-13 11:19:34 +0000

    bump to 1.0.4

--- app/src/androidTest/java/com/example/flipcoin/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/flipcoin/ExampleInstrumentedTest.kt
@@ -18,3 +18,7 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.flipcoin", appContext.packageName)
+    }
