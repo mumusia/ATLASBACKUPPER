8328bfae43626d5c6a5a36ba1d198990d9f149f3
mumusia <109430387+mumusia@users.noreply.github.com>
2020-11-05 11:36:54 +0000

    update sitemap

--- app/src/main/java/com/example/flipcoin/MainActivity.kt
+++ app/src/main/java/com/example/flipcoin/MainActivity.kt
@@ -8,3 +8,8 @@ import android.widget.ImageView
 
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val flipButton: Button = findViewById(R.id.button)
