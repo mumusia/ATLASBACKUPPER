9e784cf7bcd124f0528bc4ecc46f28396323d70f
mumusia <109430387+mumusia@users.noreply.github.com>
2020-11-05 06:49:36 +0000

    Removed some debug output.

--- app/src/main/java/com/example/flipcoin/MainActivity.kt
+++ app/src/main/java/com/example/flipcoin/MainActivity.kt
@@ -2,3 +2,9 @@
 package com.example.flipcoin
 
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+
+class MainActivity : AppCompatActivity() {
