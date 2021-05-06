66775485038c50683b1e031ff82bc083b10a2c92
mumusia <109430387+mumusia@users.noreply.github.com>
2021-05-06 14:08:27 +0000

    Update multiples.html

--- app/src/test/java/com/example/flipcoin/ExampleUnitTest.kt
+++ app/src/test/java/com/example/flipcoin/ExampleUnitTest.kt
@@ -8,3 +8,5 @@ import org.junit.Assert.*
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
