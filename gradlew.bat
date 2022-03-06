277c9938914a21c8fcb896b1470d5fdda78b14aa
mumusia <109430387+mumusia@users.noreply.github.com>
2022-03-06 05:25:09 +0000

    fix namespace reference errors

--- gradlew.bat
+++ gradlew.bat
@@ -12,4 +12,5 @@
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
-@re
+@rem
+
